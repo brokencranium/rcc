5d364225b8d1ffb2961f6ffbfdae0f12f5ab4306
vvennava <vikas.vennavali@gmail.com>
2020-02-19 16:40:16 -0800

    Updated input structure for the file

--- practice_sessions.py
+++ practice_sessions.py
@@ -1,50 +1,41 @@
 import json
+import pytz
 
 from pymongo import MongoClient
-
-from dataclasses import dataclass
-from dataclasses import field
-from typing import List
-
+from dataclasses import dataclass, asdict
 from datetime import datetime
-from datetime import date
 
 # pprint library is used to make the output look more pretty
-# from pprint import pprint
-
+from pprint import pprint
 
 # Issue the serverStatus command and print the results
 # serverStatusResult=db.command("serverStatus")
 # pprint(serverStatusResult)
-
-BALLS_COST = 55
-NETS_COST = 35
+COST_OF_BALLS = 55.00
+INSERT = "INSERT"
+UPDATE_BALLS = "UPDATE_BALLS"
+CREDIT = 'CREDIT'
+DEBIT = 'DEBIT'
+BALLS = 'BALLS'
+NETS = 'NETS'
+COST_BALLS = 'COST_BALLS'
+REPORT = 'REPORT'
 
 
 @dataclass
 class Transaction:
-    name: str
+    _id: str
+    member: str
+    date: datetime
+    tran_type: str
+    description: str
     amt: float = 0.00
+    update_ts: datetime = datetime.utcnow()
+    create_ts: datetime = datetime.utcnow()
 
 
-@dataclass
-class Member:
-    name: str
-    date: date = date.today()
-    credit_list: List[Transaction] = field(default_factory=list)
-    debit_list: List[Transaction] = field(default_factory=list)
-
-
-def default(o):
-    if isinstance(o, (date, datetime)):
-        return o.isoformat()
-
-    if isinstance(o, Transaction):
-        return o.__dict__
-
-
-def get_transactions():
-    with open('./transactions.json') as f:
+def get_transactions(path):
+    with open(path) as f:
         data = json.load(f)
 
     return data
@@ -57,38 +48,161 @@ def get_collection():
     return client.rcc.PracticeSessions
 
 
-def add_transaction(transaction_date, attendees, collection):
-    for attendee in attendees:
-        member = Member(attendee,
-                        transaction_date,
-                        credit_list=[Transaction("balls", BALLS_COST),
-                                     Transaction("nets", NETS_COST)]
-                        )
-        member_json = json.dumps(member.__dict__,
-                                 default=default
-                                 )
-
-        member_dict = json.loads(member_json)
-        collection.insert_one(member_dict)
+def add_transaction(transaction_date, attributes, collection):
+    for member in attributes['members']:
+        member_name = "".join(member.split()).upper()
+        for attribute in attributes['transactions']:
+            unique_id = member_name + transaction_date.strftime('%Y%m%d') + attribute['type'] + attribute['description']
+            transaction = Transaction(unique_id,
+                                      member_name,
+                                      transaction_date,
+                                      attribute['type'],
+                                      attribute['description'],
+                                      attribute['amt'] / len(attributes['members'])
+                                      )
+            try:
+                collection.insert_one(asdict(transaction))
+            except Exception as ex:
+                print(ex)
 
 
 def add_transactions(transactions: dict, collection):
-    for key, values in transactions.items():
-        transaction_date = datetime.strptime(key, '%Y-%m-%d').date()
+    for transaction in transactions:
+        [[key, values]] = transaction.items()
+        transaction_date = datetime.strptime(key, '%Y-%m-%d').astimezone(pytz.utc)
         add_transaction(transaction_date, values, collection)
 
 
-def update_balls_cost(collection, new_cost):
-    collection.update_many(
-        {},
+def get_cost_of_balls_session(start_date: datetime, end_date: datetime, collection):
+    counts = collection.aggregate([
         {
-            "$set": {"credit_list.$[].amt": new_cost}
-        }
-    )
+            "$match": {"tran_type": CREDIT,
+                       "description": NETS,
+                       "date": {"$gte": start_date,
+                                "$lte": end_date
+                                }
+
+                       }
+        },
+        {"$group": {"_id": {"$dateToString": {"format": "%Y-%m-%d",
+                                              "date": "$date"
+                                              }
+                            },
+                    }
+         }
+        ,
+        {"$count": "counts"}
+    ])
+
+    count = 0
+    for batch in counts:
+        count = batch['counts']
+
+    return COST_OF_BALLS / count
+
+
+def add_cost_of_balls(start_date: datetime, end_date: datetime, collection):
+    cost_per_session = get_cost_of_balls_session(start_date, end_date, collection)
+    print(f'Cost per session {cost_per_session}')
+
+    docs = collection.aggregate([
+        {
+            "$match": {"tran_type": "CREDIT",
+                       "description": 'NETS',
+                       "date": {"$gte": start_date,
+                                "$lte": end_date
+                                }
+
+                       }
+        },
+        {"$group": {"_id": {"$dateToString": {"format": "%Y-%m-%d",
+                                              "date": "$date"
+                                              }
+                            },
+                    "member": {"$push": "$$ROOT"}
+                    }
+         }
+    ])
+
+    for doc in docs:
+        cost_per_person = cost_per_session / len(doc['member'])
+        for member in doc['member']:
+            unique_id = member['member'] + member['date'].strftime('%Y%m%d') + CREDIT + BALLS
+
+            transaction = Transaction(unique_id,
+                                      member['member'],
+                                      member['date'],
+                                      CREDIT,
+                                      BALLS,
+                                      cost_per_person
+                                      )
+            collection.insert_one(asdict(transaction))
+
+
+def display_report(start_date, end_date, collection):
+    docs = collection.aggregate([{"$match": {"date": {"$gte": start_date,
+                                                      "$lte": end_date}
+                                             }
+                                  },
+                                 {"$group": {"_id": "$member",
+                                             "Credit": {
+                                                 "$sum": {
+                                                     "$switch": {
+                                                         "branches": [
+                                                             {
+                                                                 "case": {"$eq": ["$tran_type", CREDIT]},
+                                                                 "then": "$amt"
+                                                             }
+                                                         ],
+                                                         "default": 0
+                                                     }}},
+                                             "Debit": {
+                                                 "$sum": {
+                                                     "$switch": {
+                                                         "branches": [
+                                                             {
+                                                                 "case": {"$eq": ["$tran_type", DEBIT]},
+                                                                 "then": "$amt"
+                                                             }
+                                                         ],
+                                                         "default": 0
+                                                     }}}
+                                             }
+                                  }
+                                 ])
+    total_credit = 0
+    total_debit = 0
+    total_due = 0
+    for doc in docs:
+        due = doc['Credit'] - doc['Debit']
+        print(f"{doc['_id']:<15}"
+              f"Credit: {doc['Credit']:<10.2f}"
+              f"Debit: {doc['Debit']:<10.2f}"
+              f"Due: {due:<10.2f}")
+        total_credit = doc['Credit'] + total_credit
+        total_debit = doc['Debit'] + total_debit
+        total_due = due + total_due
+
+    print(f"\nTotal Credit: {total_credit:<12.2f} Total Debit:{total_debit:<12.2f} Total Due:{total_due:<12.2f}")
 
 
 if __name__ == '__main__':
+    # transaction_mode = INSERT
+    # transaction_mode = COST_BALLS
+    transaction_mode = REPORT
+
     collection = get_collection()
-    transactions = get_transactions()
-    add_transactions(transactions, collection)
-    # update_balls_cost(collection, BALLS_COST, 9.17)
+
+    if transaction_mode == INSERT:
+        transactions = get_transactions('./transactions.json')
+        add_transactions(transactions, collection)
+
+    elif transaction_mode == COST_BALLS:
+        start_date = datetime.strptime('2019-01-01', '%Y-%m-%d').astimezone(pytz.utc)
+        end_date = datetime.strptime('2020-12-31', '%Y-%m-%d').astimezone(pytz.utc)
+        add_cost_of_balls(start_date, end_date, collection)
+
+    elif transaction_mode == REPORT:
+        start_date = datetime.strptime('2019-01-01', '%Y-%m-%d').astimezone(pytz.utc)
+        end_date = datetime.strptime('2020-12-31', '%Y-%m-%d').astimezone(pytz.utc)
+        display_report(start_date, end_date, collection)
